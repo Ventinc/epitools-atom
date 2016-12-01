cf1c022f873a7334752ce4b6b151e3f5ab986d5e
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2016-12-01 10:56:46 +0100

    first commit

---
+++ spec/epitools-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EpitoolsAtomView from '../lib/epitools-atom-view';
+
+describe('EpitoolsAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
